118ef8449094a74aa98bfba94f355dd80f3201eb
Asim K <sok1864@proton.me>
2022-12-06 22:14:21 +0300

    Sayfa1 de değişiklik yaptım

--- src/Kitap/Sayfa1.java
+++ src/Kitap/Sayfa1.java
@@ -2,6 +2,6 @@ package Kitap;
 
 public class Sayfa1 {
     public static void main(String[] args) {
-        System.out.println("Githup  Deneme");
+        //System.out.println("Githup  Deneme");
     }
 }
