899bd6deb1d1e38c11427101ec53b583628783d6
Asim K <sok1864@proton.me>
2022-12-06 22:12:17 +0300

    Sayfa1 2 oluşturdum

---
+++ src/Kitap/Sayfa1.java
@@ -0,0 +1,7 @@
+package Kitap;
+
+public class Sayfa1 {
+    public static void main(String[] args) {
+        System.out.println("Githup  Deneme");
+    }
+}
---
+++ src/Kitap/Sayfa2.java
@@ -0,0 +1,7 @@
+package Kitap;
+
+public class Sayfa2 {
+    public static void main(String[] args) {
+        System.out.println("Sayfa2");
+    }
+}
