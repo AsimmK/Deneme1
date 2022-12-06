f33177d8283f781812d94e6ce3d0d21663a5c9a3
Asim K <sok1864@proton.me>
2022-12-06 22:38:33 +0300

    Sayfa 3 ekledim

--- src/Kitap/Sayfa3.java
+++ src/Kitap/Sayfa3.java
@@ -3,5 +3,6 @@ package Kitap;
 public class Sayfa3 {
     public static void main(String[] args) {
         System.out.println("Sayfa 3 oluşturdum");
+        System.out.println("Sayfa 3 neden gitmedi");
     }
 }
